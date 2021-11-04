02790dd192c12a926cc43cfe162b5c3fe561e654
Meekyle Mondal <amond049@uottawa.ca>
2021-11-03 22:06:13 -0400

    Adding the changing images, not too efficient though unfortunately, will have to change that

--- index.js
+++ index.js
@@ -5,17 +5,20 @@ var ctx = canvas.getContext("2d");
 // An array that will contain all Block instances
 var blocks = [];
 
+var current_index = 0;
+var options = ["rock", "paper", "scissors"];
 
-// Creating a Block class that will be the type of all Rock, Papers and Scissors 
+// Creating a Block class that will be the type of all Rock, Papers and Scissors
 class Block {
-    constructor(x, y, x_vel, y_vel){
+    constructor(x, y, x_vel, y_vel, classification){
         this.x_coordinate = x;
         this.y_coordinate = y;
         this.x_velocity = x_vel;
         this.y_velocity = y_vel;
+        this.classification = classification;
     }
 
-    // Creating the getter methods 
+    // Creating the getter methods
     getX() {
         return this.x_coordinate;
     }
@@ -30,6 +33,10 @@ class Block {
 
     getYVel(){
         return this.y_velocity;
+    }
+
+    getClassification(){
+        return this.classification;
     }
 
     // Creating the setter methods
@@ -48,37 +55,50 @@ class Block {
     setYVelocity(y){
         this.y_velocity = y;
     }
+
+    setClassification(c){
+        this.classification = c;
+    }
 }
 
 // Generating all the blocks
-// NEXT STEPS: MAKE IT SO THAT PEOPLE CAN ENTER HOW MANY BLOCKS THEY WOULD LIKE TO HAVE!
-for (let i = 0; i < 15; i++){
-    let x_coord = Math.trunc(Math.random() * (canvas.width - 20) + 1);
-    let y_coord = Math.trunc(Math.random() * (canvas.height - 20) + 1);
-    // NEXT STEPS: Make it so that the velocity can be either positive or negative 
-    let positive_negative_x = Math.round(Math.random());
-    let positive_negative_y = Math.round(Math.random());
-    let multiplier_x = 1;
-    let multiplier_y = 1;
-    
-    if (positive_negative_x == 0){
-        multiplier_x = -1;
-    }
-    
-    if (positive_negative_y == 0){
-        multiplier_y = -1;
-    }
-    
-    let x_velocity = Math.trunc(Math.random() * 1 + 1) * multiplier_x;
-    let y_velocity = Math.trunc(Math.random() * 1 + 1) * multiplier_y;
-    let block = new Block(x_coord, y_coord, x_velocity, y_velocity);
+function createBlocks(number){
+    blocks = [];
+    for (let i = 0; i < number; i++){
+        let x_coord = Math.trunc(Math.random() * (canvas.width - 20) + 1);
+        let y_coord = Math.trunc(Math.random() * (canvas.height - 20) + 1);
+        // NEXT STEPS: Make it so that the velocity can be either positive or negative 
+        let positive_negative_x = Math.round(Math.random());
+        let positive_negative_y = Math.round(Math.random());
+        let multiplier_x = 1;
+        let multiplier_y = 1;
+                if (positive_negative_x == 0){
+            multiplier_x = -1;
+        }
 
-    blocks.push(block);
+        if (positive_negative_y == 0){
+            multiplier_y = -1;
+        }
+
+        let x_velocity = Math.trunc(Math.random() * 1 + 1) * multiplier_x;
+        let y_velocity = Math.trunc(Math.random() * 1 + 1) * multiplier_y;
+
+        let classification = options[current_index];
+        console.log(classification);
+        if (current_index != 2){
+            current_index += 1
+        } else {
+            current_index = 0;
+        }
+
+        let block = new Block(x_coord, y_coord, x_velocity, y_velocity, classification);
+
+        blocks.push(block);
+    }
 }
-
 // Moving the blocks around the screen
 function moveRectangle(blocks){
-    
+
     // Checking to see if the blocks are colliding with the borders of the canvas
     blocks.forEach(block => {
         if (block.getX() + 20 < canvas.width && block.getX() > 0){
@@ -90,11 +110,11 @@ function moveRectangle(blocks){
             } else {
                 block.setX(0);
             }
-            
+
             block.setXVelocity(-block.getXVel());
             block.setX(block.getX() + block.getXVel());
         }
-    
+
         if (block.getY() + 20 < canvas.height && block.getY() > 0){
             block.setY(block.getY() + block.getYVel());
         } else{
@@ -109,27 +129,50 @@ function moveRectangle(blocks){
         }
     })
 
-    // Checking to see if the blocks are colliding with each other. Not that efficient of a solution as it is O(n^2) algorithm, but I don't imagine 
-    // overloading the application with so many blocks it won't be able to hand it all
+    // Need to make the checking if they are colliding function much more efficient, cannot keep up with the changing images
     for (let i = 0; i < blocks.length; i++){
         let current_x = blocks[i].getX();
         let current_y = blocks[i].getY();
- 
+
         for (let j = 0; j < blocks.length; j++){
             if (j != i ){
                 let new_x = blocks[j].getX();
                 let new_y = blocks[j].getY();
-                
-                
-                if (Math.abs(new_x - current_x) <= 20 && Math.abs(new_y - current_y) <= 20){
-                    
+
+
+                if (Math.abs(new_x - current_x) <= 25 && Math.abs(new_y - current_y) <= 25){
+                    // Need to switch based on the results 
+                    if (blocks[i].getClassification() == "rock" && blocks[j].getClassification() == "paper"){
+                        blocks[i].setClassification("paper");
+                    } else if (blocks[i].getClassification() == "rock" && blocks[j].getClassification() == "scissors"){
+                        console.log(blocks[j].getClassification());
+                        blocks[j].setClassification("rock");
+                        console.log(blocks[j].getClassification());
+                    }
+
+                    if (blocks[i].getClassification() == "paper" && blocks[j].getClassification() == "scissors"){
+                        blocks[i].setClassification("scissors");
+                    } else if (blocks[i].getClassification() == "paper" && blocks[j].getClassification() == "rock"){
+                        console.log(blocks[j].getClassification());
+                        blocks[j].setClassification("paper");
+                        console.log(blocks[j].getClassification());
+                    }
+
+                    if (blocks[i].getClassification() == "scissors" && blocks[j].getClassification() == "paper"){
+                        blocks[i].setClassification("scissors");
+                    } else if (blocks[i].getClassification() == "scissors" && blocks[j].getClassification() == "rock"){
+                        console.log(blocks[j].getClassification());
+                        blocks[j].setClassification("rock");
+                        console.log(blocks[j].getClassification());
+                    }
+
                     // Perfectly elastic collision
                     let tempx = blocks[i].getXVel();
                     let tempy = blocks[i].getYVel();
 
                     blocks[i].setXVelocity(blocks[j].getXVel());
                     blocks[i].setYVelocity(blocks[j].getYVel())
-                     
+
                     blocks[j].setXVelocity(tempx);
                     blocks[j].setYVelocity(tempy);
 
@@ -149,11 +192,30 @@ function drawRectangle(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "#666";
     
-    blocks.forEach(block => ctx.fillRect(block.getX(), block.getY(), 20, 20));
+    
+
+    blocks.forEach(block =>{
+        var image = new Image();
+        image.src = block.getClassification() + ".png";
+        
+        ctx.drawImage(image, block.getX(), block.getY(), 20, 20)
+    
+    }
+    
+    );
     moveRectangle(blocks);
 }
 
 // Refreshing the screen every 10 ms
 setInterval(function(){drawRectangle()}, 10);
 
+//========================================================================= GENERAL JS ============================================================================
+var slider = document.getElementById("blockRange");
+var initialAmount = slider.value;
+createBlocks(initialAmount);
 
+slider.oninput = function(){
+    createBlocks(this.value);
+    console.log(this.value);
+    document.getElementById("numberOfBlocks").innerHTML = this.value + " blocks!";
+}
